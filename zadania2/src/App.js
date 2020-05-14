9c9ccd80007d8c72d74c67115720fae02f93a12f
gbonar <grzegorz.bonar@pracuj.pl>
2020-05-14 20:25:51 +0200

    Wyjaśnienie zmian danych stanu 30

--- zadania2/src/App.js
+++ zadania2/src/App.js
@@ -3,12 +3,29 @@ import React, { Component } from 'react';
 //import './App.css';
 
 export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      userName: "Adam"
+    }
+  }
+
+  changeStateData = () => {
+    this.setState({
+      userName: this.state.userName === "Adam" ? "Jakub" : "Adam"
+    })
+  }
+
   render() {  
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
-          Lista zadań
+          Lista zadań użytkownika {this.state.userName}
         </h4>
+        <button className="btn btn-primary m-2" onClick={this.changeStateData}>
+          Zmień
+        </button>
       </div>
     )
   };
