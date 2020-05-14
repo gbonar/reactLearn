1a41e56faa7c60e45aa8d9e25a77956ee90b6ee6
gbonar <grzegorz.bonar@pracuj.pl>
2020-05-14 21:22:33 +0200

    Dodawanie możliwości aplikacji listy zadań 33

--- zadania2/src/App.js
+++ zadania2/src/App.js
@@ -7,7 +7,12 @@ export default class App extends Component {
   constructor(props){
     super(props);
     this.state = {
-      userName: "Adam"
+      userName: "Adam",
+      todoItems:[{ action: "Kupić kwiaty", done: false },
+                { action: "Wziąć buty", done: false },
+                { action: "Zebrać bilety", done: true },
+                { action: "Zadzwonić do Jurka", done: false }],
+      newItemText: ""
     }
   }
 
@@ -17,12 +22,38 @@ export default class App extends Component {
     })
   }
 
+  updateNewTextValue = (event) => {
+    this.setState({ newItemText: event.target.value })
+  }  
+
+  createNewTodo = () => {
+    var exist = this.state.todoItems
+      .find(item => item.action === this.state.newItemText)
+
+    if (!exist)
+    {
+      this.setState({ 
+        todoItems: [...this.state.todoItems, 
+          { action: this.state.newItemText, done: false} ], newItemText: ""});
+    }
+  }
+
   render() {  
     return (
       <div>
         <h4 className="bg-primary text-white text-center p-2">
           Lista zadań użytkownika {this.state.userName}
+          (Liczba zadań: {this.state.todoItems.filter(t => !t.done).length})
         </h4>
+
+        <div className="container-fluid">
+          <div className="my-1">
+            <input className="form-control" value={this.state.newItemText} onChange={this.updateNewTextValue}></input>
+            <button className="btn btn-primary mt-1" onClick={this.createNewTodo}>Dodaj</button>
+          </div>
+
+        </div>
+
         <button className="btn btn-primary m-2" onClick={this.changeStateData}>
           Zmień
         </button>
