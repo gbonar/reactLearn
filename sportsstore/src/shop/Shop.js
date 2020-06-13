b58c9a150e7e111593ee1e6b46bce1e09ef42f49
gbonar <grzegorz.bonar@pracuj.pl>
2020-06-13 10:51:52 +0200

    Zaczac od: Dodawanie komponentu szczegółów koszyka str 141

--- sportsstore/src/shop/Shop.js
+++ sportsstore/src/shop/Shop.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { CategoryNavigation } from "./CategoryNavigation";
 import { ProductList } from "./ProductList";
+import { CartSummary } from "./CartSummary";
 
 export class Shop extends Component {
     render() {
@@ -8,6 +9,7 @@ export class Shop extends Component {
             <div className="row">
                 <div className="col bg-dark text-white">
                     <div className="navbar-brand">Sklep SPORTSSTORE</div>
+                    <CartSummary { ...this.props } />
                 </div>
             </div>
             <div className="row">
@@ -16,7 +18,7 @@ export class Shop extends Component {
                         categories={this.props.categories} />
                 </div>
                 <div className="col-9 p-2">
-                    <ProductList products={this.props.products} />
+                    <ProductList products={this.props.products} addToCart={ this.props.addToCart } />
                 </div>
             </div>
         </div>
