14a6dcc26013893bfc6a955774602140136e47f2
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-06 07:36:52 +0200

    str 263: Właściwości props

---
+++ components/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Message } from "./Message";
+import { Summary } from "./Summary";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="bg-primary text-white text-center p-2">
+        Witaj, Adamie!
+  </h1>
+  <Message />
+<Message />
+<Message />
+<Summary />
+    </div>
+  )
+}
+
