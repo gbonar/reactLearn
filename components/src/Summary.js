697dea5c2ea66326ffc245e9e6d53d2a99e57490
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-08 10:22:15 +0200

    str 270: Dodawanie właściwości props key

--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,22 +1,13 @@
 import React from "react";
 
-function createInnerElements(names) {
-    let arrayElems = [];
-
-    for(let i = 0; i < names.length; i++) {
-        arrayElems.push(
-            <div>
-                {`"${names[i]}" składa sie z ${names[i].length} znaków`}
-            </div>
-        )
-    }
-
-    return arrayElems;
-}
-
 export function Summary(props) {
     return <h4 className="bg-info text-white text-center p-2">
-        
-        { createInnerElements(props.names) }
-</h4>
+
+        {props.names.map(name =>
+            <div>
+                {`"${name}" składa się z ${name.length} znaków`}
+            </div>
+        )
+        }
+    </h4>
 }
