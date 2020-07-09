199f56dfad6dee5bfa99ba26bb37a30474485504
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-09 09:03:37 +0200

    str 280: Unikanie problemu przedwczesnego wywołania

--- components/src/App.js
+++ components/src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
 import { Summary } from "./Summary";
+import ReactDOM from "react-dom";
 
 let names = ["Jarek", "Ala", "Marek"];
+
+function reverseNames() {
+  names.reverse();
+  ReactDOM.render(<App/>, document.getElementById('root'))
+}
+
+function promoteName(name) {
+  names = [name, ...names.filter(val => val !== name)];
+  ReactDOM.render(<App />, document.getElementById('root'));
+}
 
 export default function App() {
   return (
@@ -12,7 +23,9 @@ export default function App() {
       <tbody>
         {names.map((name, index) =>
           <tr key={name}>
-            <Summary index={index} name={name} />
+            <Summary index={index} name={name} 
+              reverseCallback={reverseNames}
+              promoteCallback={promoteName} />
           </tr>
         )}
       </tbody>
--- components/src/Summary.js
+++ components/src/Summary.js
@@ -1,13 +1,19 @@
 import React from "react";
 
 export function Summary(props) {
-    if(props.name.length < 4) {
-        return null;
-    }
-    
     return <React.Fragment>
             <td>{props.index + 1} </td>
             <td>{props.name} </td>
             <td>{props.name.length} </td>
+
+            <td>
+                <button className="btn btn-primary btn-sm" onClick={props.reverseCallback}>
+                    Zmień kolejność
+                </button>
+                <button className="btn btn-info btn-sm m-1" onClick={() => props.promoteCallback(props.name)}>
+                    Na początek
+                </button>
+            </td>
+
         </React.Fragment>
 }
