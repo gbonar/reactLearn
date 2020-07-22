7b29b44fa869b23c261749c057a0addf1686f284
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-22 08:43:08 +0200

    str 343: Rekoncyliacja i cykl życia

--- reactevents/src/App.js
+++ reactevents/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { ThemeButton } from "./ThemeButton";
 
 export default class App extends Component {
+  
   constructor(props) {
     super(props);
     this.state = {
@@ -10,6 +12,19 @@ export default class App extends Component {
     }
   }
 
+  selectTheme = (newTheme) => {
+    this.setState({
+    theme: newTheme,
+    message: `Theme: ${newTheme}`
+    });
+    }
+
+    handleClick = (event) => {
+      console.log(`App: Type: ${event.type} `
+      + `Target: ${event.target.tagName} `
+      + `CurrentTarget: ${event.currentTarget.tagName}`);
+    }
+
   handleEvent = (event, newTheme) => {
     event.persist();
     this.setState({
@@ -18,8 +33,19 @@ export default class App extends Component {
     }, () => this.setState({ message: `${event.type}: ${this.state.counter}` }));
   }
 
+toggleCheckBox = (event) => {
+  if(this.state.counter === 0) {
+    event.preventDefault();
+  }
+}
+
   render() {
-    return <div className="m-2">
+    return <div className="m-2" onClick={ this.handleClick }>
+      <div className="form-check">
+        <input className="form-check-input" type="checkbox"
+          onClick={this.toggleCheckBox }/>
+          <label>To jest pole wyboru</label>
+      </div>
       <div className={`h4 bg-${this.state.theme}
         text-white text-center p-2`}>
         {this.state.message}
@@ -33,6 +59,10 @@ export default class App extends Component {
           onClick={(e) => this.handleEvent(e, "danger")} >
           uwaga!
       </button>
+      </div>
+      <div className="text-center" onClick = { this.handleClick }>
+        <ThemeButton theme="primary" callback={this.selectTheme} />
+        <ThemeButton theme="danger" callback={this.selectTheme} />
       </div>
     </div>
   }
