737ffd478f783cb261fba19cced5d6a727b7a73e
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-24 08:09:31 +0200

    str: 373 Konstruowanie aplikacji

--- lifecycle/src/App.js
+++ lifecycle/src/App.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
 import { Message } from "./Message";
-import { List } from "./List";
-import { ExternalCounter } from './ExternalCounter';
+//import { List } from "./List";
+//import { ExternalCounter } from './ExternalCounter';
 
 export default class App extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      counterLeft: 0,
+      counterRight: 0
+    }
+  }
+  incrementCounter = (counter) => {
+    if (counter === "left") {
+      this.setState({ counterLeft: this.state.counterLeft + 1 });
+    } else {
+      this.setState({ counterRight: this.state.counterRight + 1 });
     }
   }
 
@@ -16,23 +24,29 @@ export default class App extends Component {
     this.setState({ counter: this.state.counter + 1 });
   }
 
+  handleChange = () => {
+    this.setState({ showMessage: !this.state.showMessage });
+  }
+
   render() {
     console.log("Komponent App, metoda render.");
-
-    return <div className="container text-center">
-      <div className="row p-2">
-        <div className="col-4">
-          <Message message={`Licznik: ${this.state.counter}`}
-            callback={this.incrementCounter}
-            text="Inkrementuj licznik" />
-        </div>
-        <div className="col-4">
-          <List />
-        </div>
-        <div className="col-4">
-          <ExternalCounter />
+    return (
+      <div className="container text-center">
+        <div className="row p-2">
+          <div className="col-6">
+            <Message
+              message={`Lewy: ${this.state.counterLeft}`}
+              callback={() => this.incrementCounter("left")}
+              text="Inkrementuj lewy licznik" />
+          </div>
+          <div className="col-6">
+            <Message
+              message={`Prawy: ${this.state.counterRight}`}
+              callback={() => this.incrementCounter("right")}
+              text="Inkrementuj prawy licznik" />
+          </div>
         </div>
       </div>
-    </div>
+    )
   }
 }
--- lifecycle/src/Message.js
+++ lifecycle/src/Message.js
@@ -2,26 +2,26 @@ import React, { Component } from "react";
 import { ActionButton } from "./ActionButton";
 
 export class Message extends Component {
-    
+
     constructor(props) {
         super(props);
         this.state = {
             showSpan: false
         }
     }
-    
+
     handleClick = (event) => {
         this.setState({ showSpan: !this.state.showSpan });
         this.props.callback(event);
     }
-    
+
     getMessageElement() {
         let div = <div id="messageDiv" className="h5 text-center p-2">
             {this.props.message}
         </div>
         return this.state.showSpan ? <span>{div} </span> : div;
     }
-    
+
     render() {
         console.log(`Komponent Message, metoda render.`);
         return (
@@ -31,5 +31,16 @@ export class Message extends Component {
                 {this.getMessageElement()}
             </div>
         )
+    }
+
+    shouldComponentUpdate(newProps, newState) {
+        let change = newProps.message !== this.props.message;
+        if (change) {
+            console.log(`shouldComponentUpdate ${this.props.text}: aktualizacja możliwa.`)
+        } else {
+            console.log(
+                `shouldComponentUpdate ${this.props.text}: aktualizacja niedozwolona.`)
+        }
+        return change;
     }
 }
