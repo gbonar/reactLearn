62abea3c5c5400e62794d9227af15b7491673d94
gbonar <grzegorz.bonar@pracuj.pl>
2020-07-25 10:16:43 +0200

    str 383: Tworzenie komponentów wyspecjalizowanych

---
+++ composition/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { Message } from "./Message";
+import { ActionButton } from './ActionButton';
+import { ThemeSelector } from './ThemeSelector';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  incrementCounter = () => {
+    this.setState({ counter: this.state.counter + 1 });
+  }
+
+  render() {
+    return <div className="m-2 text-center">
+      <ThemeSelector>
+        <Message theme="primary"
+          message={`Licznik: ${this.state.counter}`} />
+        <ActionButton theme="secondary"
+          text="Inkrementuj" callback={this.incrementCounter} />
+        </ThemeSelector>
+    </div>
+  }
+}
