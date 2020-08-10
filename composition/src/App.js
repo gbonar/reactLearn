ff9356de0e65def45b442ef6e5d742026078d680
gbonar <grzegorz.bonar@pracuj.pl>
2020-08-10 20:27:20 +0200

    str: 385 - Tworzenie komponentów wyższego rzędu

--- composition/src/App.js
+++ composition/src/App.js
@@ -1,28 +1,35 @@
 import React, { Component } from 'react';
-import { Message } from "./Message";
-import { ActionButton } from './ActionButton';
-import { ThemeSelector } from './ThemeSelector';
+//import { Message } from "./Message";
+//import { ActionButton } from './ActionButton';
+//import { ThemeSelector } from './ThemeSelector';
+import { GeneralList } from './GeneralList';
+import { SortedList } from "./SortedList";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      counter: 0
+      //counter: 0
+      names: ["Zosia", "Kuba", "Ala", "Ola", "Jaś"]
     }
   }
 
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  }
+  //incrementCounter = () => {
+  //this.setState({ counter: this.state.counter + 1 });
+  //}
 
   render() {
-    return <div className="m-2 text-center">
-      <ThemeSelector>
-        <Message theme="primary"
-          message={`Licznik: ${this.state.counter}`} />
-        <ActionButton theme="secondary"
-          text="Inkrementuj" callback={this.incrementCounter} />
-        </ThemeSelector>
-    </div>
+    return (
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-6">
+            <GeneralList list={this.state.names} theme="primary" />
+          </div>
+          <div className="col-6">
+            <SortedList list={this.state.names} />
+          </div>
+        </div>
+      </div>
+    )
   }
 }
