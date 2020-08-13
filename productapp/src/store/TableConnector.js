0fdb1bcbf16c08a0c4eb54eacf439b698867891d
gbonar <grzegorz.bonar@pracuj.pl>
2020-08-13 12:41:53 +0200

    str 625: Korzystanie z usługi internetowej typu RESTful

--- productapp/src/store/TableConnector.js
+++ productapp/src/store/TableConnector.js
@@ -4,14 +4,36 @@ import { deleteProduct, deleteSupplier } from "./modelActionCreators";
 import { PRODUCTS, SUPPLIERS } from "./dataTypes";
 
 export const TableConnector = (dataType, presentationComponent) => {
-    const mapStateToProps = (storeData) => ({
-        products: storeData.modelData[PRODUCTS],
-        suppliers: storeData.modelData[SUPPLIERS]
-    })
-    const mapDispatchToProps = {
-        editCallback: dataType === PRODUCTS
-            ? startEditingProduct : startEditingSupplier,
-        deleteCallback: dataType === PRODUCTS ? deleteProduct : deleteSupplier
+
+  const mapStateToProps = (storeData, ownProps) => {
+    if (!ownProps.needSuppliers) {
+      return { products: storeData.modelData[PRODUCTS] };
+    } else {
+      return {
+        suppliers: storeData.modelData[SUPPLIERS].map(supp => ({
+          ...supp,
+          products: supp.products.map(id =>
+            storeData.modelData[PRODUCTS]
+              .find(p => p.id === Number(id)) || id)
+              .map(val => val.name || val)
+        }))
+      }
     }
-    return connect(mapStateToProps, mapDispatchToProps)(presentationComponent);
+  }
+
+  const mapDispatchToProps = (dispatch, ownProps) => {
+    if (!ownProps.needSuppliers) {
+      return {
+        editCallback: (...args) => dispatch(startEditingProduct(...args)),
+        deleteCallback: (...args) => dispatch(deleteProduct(...args))
+      }
+    } else {
+      return {
+        editCallback: (...args) => dispatch(startEditingSupplier(...args)),
+        deleteCallback: (...args) => dispatch(deleteSupplier(...args))
+      }
+    }
+  }
+
+  return connect(mapStateToProps, mapDispatchToProps)(presentationComponent);
 }
